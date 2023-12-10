0b5d36f4dadd309167fa9f4e8ddf410a9933d853
yelena.smolenchuk <helen.smolench@gmail.com>
2023-12-10 21:09:38 +0300

    HW-1

--- src/main/java/ru/mts/service/Main.java
+++ src/main/java/ru/mts/service/Main.java
@@ -1,22 +1,28 @@
 package ru.mts.service;
 
-import ru.mts.dto.House;
+import ru.mts.dao.Calculating;
+import ru.mts.model.Purchase;
 
 public class Main {
+
+    // Создание объекта Calculating для вызова методов calculateTotalWithoutDiscount() и calculateTotalWithDiscount
+    private static Calculating calculate;
+
     public static void main(String[] args) {
 
-        House woodHouse = new House();
-        woodHouse.setMaterial("wood");
+        // Создание объекта Purchase с 10 товарами, суммой 20 и скидкой 0.75%
+        Purchase purchaseFirst = new Purchase(10, 20, 0.75);
+        calculate.calculateTotalWithoutDiscount(purchaseFirst);
+        calculate.calculateTotalWithDiscount(purchaseFirst);
 
-        updateMaterial(woodHouse);
-        System.out.println(woodHouse);
+        // Создание объекта Purchase с 5 товарами, суммой 30 и скидкой 42.575%
+        Purchase purchaseSecond = new Purchase(5, 30, 42.575);
+        calculate.calculateTotalWithoutDiscount(purchaseSecond);
+        calculate.calculateTotalWithDiscount(purchaseSecond);
 
-    }
-
-    public static void updateMaterial(House house) {
-        house.setMaterial("lflfl");
-
-        house = new House();
-        house.setMaterial("hehehe");
+        // Создание объекта Purchase с 8 товарами, суммой 15 и скидкой 59.1%
+        Purchase purchaseThird = new Purchase(8, 15, 59.1);
+        calculate.calculateTotalWithoutDiscount(purchaseThird);
+        calculate.calculateTotalWithDiscount(purchaseThird);
     }
 }
