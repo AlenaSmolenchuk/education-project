de95112f6d2210e89d347cd2b6c0530fbd60cf3c
yelena.smolenchuk <helen.smolench@gmail.com>
2023-12-10 21:10:07 +0300

    HW-1

---
+++ src/main/java/ru/mts/model/Purchase.java
@@ -0,0 +1,89 @@
+package ru.mts.model;
+
+public class Purchase {
+
+    // Количество товаров
+    private int quantity;
+
+    // Сумма товара
+    private double amount;
+
+    // Скидка на товар
+    private double discount;
+
+    /**
+     * Конструктор класса Purchase.
+     *
+     * @param quantity Количество товаров
+     * @param amount   Сумма товара
+     * @param discount Скидка на товар
+     */
+    public Purchase(int quantity, double amount, double discount) {
+        this.quantity = quantity;
+        this.amount = amount;
+        this.discount = discount;
+    }
+
+    /**
+     * Метод для получения значения количества товаров.
+     *
+     * @return Количество товаров.
+     */
+    public int getQuantity() {
+        return quantity;
+    }
+
+    /**
+     * Метод для установки значения количества товаров.
+     *
+     * @param quantity Новое значение количества товаров.
+     */
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    /**
+     * Метод для получения значения суммы товара.
+     *
+     * @return Сумма товара.
+     */
+    public double getAmount() {
+        return amount;
+    }
+
+    /**
+     * Метод для установки значения суммы товара.
+     *
+     * @param amount Новое значение суммы товара.
+     */
+    public void setAmount(double amount) {
+        this.amount = amount;
+    }
+
+    /**
+     * Метод для получения значения скидки на товар.
+     *
+     * @return Скидка на товар.
+     */
+    public double getDiscount() {
+        return discount;
+    }
+
+    /**
+     * Метод для установки значения скидки на товар.
+     *
+     * @param discount Новое значение скидки на товар.
+     */
+    public void setDiscount(double discount) {
+        this.discount = discount;
+    }
+
+    @Override
+    public String toString() {
+        return "Purchase{" +
+                "quantity=" + quantity +
+                ", amount=" + amount +
+                ", discount=" + discount +
+                '}';
+    }
+}
