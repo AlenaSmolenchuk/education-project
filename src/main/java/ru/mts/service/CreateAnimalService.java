25e94c418233453da759816df4efb72061d57fb8
Alena <124808324+AlenaSmolenchuk@users.noreply.github.com>
2023-12-26 15:30:42 +0300

    Update CreateAnimalService.java

--- src/main/java/ru/mts/service/CreateAnimalService.java
+++ src/main/java/ru/mts/service/CreateAnimalService.java
@@ -23,20 +23,4 @@ public interface CreateAnimalService {
     default Animal createRandomAnimal(AnimalFactory animalFactory) {
         return animalFactory.createRandomAnimal();
     }
-
-    /**
-     * Проверяет, содержится ли животное в массиве.
-     *
-     * @param animals массив животных
-     * @param animal  проверяемое животное
-     * @return true, если животное содержится в массиве, иначе false
-     */
-    default boolean containsAnimal(Animal[] animals, Animal animal) {
-        for (Animal a : animals) {
-            if (a != null && a.equals(animal)) {
-                return true;
-            }
-        }
-        return false;
-    }
 }
