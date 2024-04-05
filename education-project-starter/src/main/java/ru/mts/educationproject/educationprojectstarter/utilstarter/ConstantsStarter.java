c13187990510d7eeb58a5f0039a5090ac357847d
yelena.smolenchuk <helen.smolench@gmail.com>
2024-04-05 17:31:43 +0300

    HW-13

--- education-project-starter/src/main/java/ru/mts/educationproject/educationprojectstarter/utilstarter/ConstantsStarter.java
+++ education-project-starter/src/main/java/ru/mts/educationproject/educationprojectstarter/utilstarter/ConstantsStarter.java
@@ -1,7 +1,9 @@
 package ru.mts.educationproject.educationprojectstarter.utilstarter;
 
-public class ConstantsStarter {
+public final class ConstantsStarter {
+    public ConstantsStarter() {
+    }
 
-    public static final String LOG_DATA_RESULT = "src/main/resources/animals/logData.txt";
-    public static final String SECRET_INFORMATION_RESULT = "src/main/resources/secretStore/secretInformation.txt";
+    public static final String LOG_DATA_RESULT = "classpath:animals/logData.txt";
+    public static final String SECRET_INFORMATION_RESULT = "classpath:secretStore/secretInformation.txt";
 }
