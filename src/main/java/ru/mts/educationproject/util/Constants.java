b0d9073920fb4c1928ba38fecb7f764483f190e0
roman <rameshha@mts.ru>
2024-04-05 17:13:11 +0300

    some improvements

--- src/main/java/ru/mts/educationproject/util/Constants.java
+++ src/main/java/ru/mts/educationproject/util/Constants.java
@@ -1,8 +1,10 @@
 package ru.mts.educationproject.util;
 
-public class Constants {
+public final class Constants {
 
-    public static final String RESULTS_DIRECTORY = "src/main/resources/results/";
+    private Constants() {}
+
+    public static final String RESULTS_DIRECTORY = "classpath:results/";
     public static final String FIND_LEAP_YEAR_NAMES = RESULTS_DIRECTORY + "findLeapYearNames.json";
     public static final String FIND_OLDER_ANIMALS = RESULTS_DIRECTORY + "findOlderAnimals.json";
     public static final String FIND_DUPLICATE = RESULTS_DIRECTORY + "findDuplicate.json";
